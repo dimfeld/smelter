2c62c59ee04bbc3a8ca6a96ed282b13eaa71e48c
Daniel Imfeld <daniel@imfeld.dev>
2024-02-27 16:12:45 -1000

    add stub fargate adapter

---
+++ crates/aws-fargate/src/spawner.rs
@@ -0,0 +1,40 @@
+use std::borrow::Cow;
+
+use async_trait::async_trait;
+use error_stack::Report;
+
+use super::{SpawnedTask, TaskError};
+use crate::manager::SubtaskId;
+
+pub struct LambdaSpawner {
+    s3_output_path: String,
+}
+
+#[async_trait]
+impl LambdaSpawner {
+    async fn spawn(
+        &self,
+        task_id: SubtaskId,
+        task_name: Cow<'static, str>,
+        input: Vec<u8>,
+    ) -> Result<Box<dyn SpawnedTask>, Report<TaskError>> {
+        todo!()
+    }
+}
+
+pub struct SpawnedLambda {}
+
+#[async_trait]
+impl SpawnedTask for SpawnedLambda {
+    async fn runtime_id(&self) -> Result<String, TaskError> {
+        todo!()
+    }
+
+    async fn wait(&mut self) -> Result<Vec<u8>, Report<TaskError>> {
+        todo!()
+    }
+
+    async fn kill(&mut self) -> Result<(), Report<TaskError>> {
+        todo!()
+    }
+}
