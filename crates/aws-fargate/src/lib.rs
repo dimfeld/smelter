bb516fb7bf60c871953ed2818370b93fb4bb6e78
Daniel Imfeld <daniel@imfeld.dev>
2024-02-28 10:46:20 -1000

    implement input and output payload operations in spawner

--- crates/aws-fargate/src/lib.rs
+++ crates/aws-fargate/src/lib.rs
@@ -5,3 +5,15 @@ pub mod worker;
 
 const INPUT_LOCATION_VAR: &str = "INPUT_LOCATION";
 const OUTPUT_LOCATION_VAR: &str = "OUTPUT_LOCATION";
+
+pub fn parse_s3_url(path: &str) -> Option<(String, String)> {
+    let u = url::Url::parse(path).ok()?;
+    if u.scheme() != "s3" {
+        return None;
+    }
+
+    let bucket = u.host_str()?.to_string();
+    let path = u.path().to_string();
+
+    Some((bucket, path))
+}
